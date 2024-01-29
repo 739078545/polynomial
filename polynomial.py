81c54349f2c25fe57ad7277d52a4a36cba2fd6ad
739078545 <lizihan@bu.edu>
2024-01-28 19:03:11 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -19,6 +19,19 @@ class Add:
 
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        return repr(self.p1) + " - " + self._format_p2(self.p2)
+
+    def _format_p2(self, p2):
+        if isinstance(p2, (Add, Sub)):
+            return "( " + repr(p2) + " )"
+        return repr(p2)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -26,13 +39,35 @@ class Mul:
         self.p2 = p2
 
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " * " + repr(self.p2)
+        return self._format_p1(self.p1) + " * " + self._format_p2(self.p2)
+
+    def _format_p1(self, p1):
+        if isinstance(p1, (Add, Sub)):
+            return "( " + repr(p1) + " )"
+        return repr(p1)
+
+    def _format_p2(self, p2):
+        if isinstance(p2, (Add, Sub)):
+            return "( " + repr(p2) + " )"
+        return repr(p2)
+    
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        return self._format_p1(self.p1) + " / " + self._format_p2(self.p2)
+
+    def _format_p1(self, p1):
+        if isinstance(p1, (Add, Sub, Mul)):
+            return "( " + repr(p1) + " )"
+        return repr(p1)
+
+    def _format_p2(self, p2):
+        if isinstance(p2, (Add, Sub, Mul, Div)):
+            return "( " + repr(p2) + " )"
+        return repr(p2)
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
